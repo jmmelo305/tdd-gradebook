3759a0e1e07f11581d2a7b08a115d3decc7600c2
Melo <jmmelo.edu@gmail.com>
2026-04-22 08:06:34 -0400

    Added type test for is_passing

--- test_gradebook.py
+++ test_gradebook.py
@@ -19,4 +19,8 @@ def test_is_passing_true():
     assert is_passing(75) == True 
 
 def test_is_passing_false():
-    assert is_passing(45) == False 
+    assert is_passing(45) == False
+
+def test_is_passing_invalid_type():
+    with pytest.raises(TypeError):
+        is_passing("passing")
