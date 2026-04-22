f94a8b417874acdda90323d60e786af7010e977a
Melo <jmmelo.edu@gmail.com>
2026-04-22 08:34:11 -0400

    Implemented handling for mixed scores for average function

--- gradebook.py
+++ gradebook.py
@@ -23,6 +23,8 @@ def average (scores):
         raise TypeError ("Scores must be a list")
     if len(scores) == 0:
         raise ValueError("Scores Lists Can Not Be Empty")
+    if not all(isinstance(s, (int, float))for s in scores):
+        raise TypeError ("All scores must be numbers")
     return round(sum(scores) / len(scores), 2)
 
 def curved_score(score, bonus):
