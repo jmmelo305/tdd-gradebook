90a67831a991a43db847781a8e9240c4bba90441
Melo <jmmelo.edu@gmail.com>
2026-04-22 08:48:37 -0400

    Implemented handling error for type check for score & bonus

--- gradebook.py
+++ gradebook.py
@@ -28,6 +28,8 @@ def average (scores):
     return round(sum(scores) / len(scores), 2)
 
 def curved_score(score, bonus):
+    if not isinstance(score,(int, float)) or not isinstance(bonus,(int, float)):
+        raise TypeError ("Score or Bonus should be numbers")
     if bonus < 0:
         raise ValueError ("Bonus can't be negative")
     return min(score + bonus, 100)
