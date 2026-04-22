485cac9c47b3fbecd8992a15bb87be3069dd3a73
Melo <jmmelo.edu@gmail.com>
2026-04-22 08:29:33 -0400

    Added test_average mixed data types

--- test_gradebook.py
+++ test_gradebook.py
@@ -35,3 +35,7 @@ def test_average_empty_list():
 def test_average_not_a_list():
     with pytest.raises(TypeError):
         average("not a list")
+
+def test_average_bad_items():
+    with pytest.raises(TypeError):
+        average([80, "ninety", 70])
