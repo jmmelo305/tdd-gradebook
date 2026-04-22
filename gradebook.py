ba0a631cc6daf8274ba508e456058a4fbd6dd700
Melo <jmmelo.edu@gmail.com>
2026-04-22 08:22:14 -0400

    Implemented check for empty list

--- gradebook.py
+++ gradebook.py
@@ -19,6 +19,8 @@ def is_passing (score):
     return score >= 60 
 
 def average (scores):
+    if len(scores) == 0:
+        return ValueError("Scores Lists Can Not Be Empty")
     return round(sum(scores) / len(scores), 2)
 
 def curved_score(score, bonus):
